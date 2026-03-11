acace7e89ad56e146e00c2e10beefe1b37e5a8e6
Jakub Brzozowski <brzozowski2004@gmail.com>
2026-03-11 13:40:59 +0100

    Implement basic views and templates for blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from blog.models import Post 
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'postdetailview_detail.html'
+    
+class ArticleListView(ListView):
+    model = Post
+    template_name = 'postlistview_detail.html'
+    
+
