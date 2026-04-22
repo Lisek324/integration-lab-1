6aec0f8e902fe31827d18396a99f615af8378a5e
Jakub Brzozowski <brzozowski2004@gmail.com>
2026-04-22 18:26:15 +0200

    Configure GitHub Actions CI pipeline

--- tests/test_views.py
+++ tests/test_views.py
@@ -5,4 +5,4 @@ from django.test import TestCase
 class TestBlogPage(TestCase):
     def test_blog_page_status_code(self):
         response = self.client.get('/blog/')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
